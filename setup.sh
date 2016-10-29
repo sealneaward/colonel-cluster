1c6221eae5f2404bc6127892797db95e737b5763
sealneaward <sealneaward@github.com>
2016-10-29 14:51:37 -0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+apt-get install python-pip python-dev build-essential
+apt-get install python-psycopg2 python-sklearn python-matplotlib
+apt-get install postgresql
+apt-get install pgadmin3
+
+pip install virtualenv virtualenvwrapper
