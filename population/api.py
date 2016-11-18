a2353556f37a9643a141b0a709765e3920c845c1
sealneaward <sealneaward@github.com>
2016-11-18 18:02:51 -0500

    Added player and limited to 15-16 playoffs

--- population/api.py
+++ population/api.py
@@ -6,7 +6,7 @@ def get_rim_protection():
     url = 'http://stats.nba.com/stats/leaguedashptstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=' \
           '&DraftPick=&DraftYear=&GameScope=&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0' \
           '&Outcome=&PORound=0&PerMode=Totals&PlayerExperience=&PlayerOrTeam=Player&PlayerPosition=' \
-          '&PtMeasureType=Defense&Season=2015-16&SeasonSegment=&SeasonType=Regular+Season&StarterBench=' \
+          '&PtMeasureType=Defense&Season=2015-16&SeasonSegment=&SeasonType=Playoffs&StarterBench=' \
           '&TeamID=0&VsConference=&VsDivision=&Weight='
     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     while response.status_code != 200:
@@ -22,8 +22,7 @@ def get_opponent_shooting_by_zone():
           '&DistanceRange=By+Zone&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0' \
           '&LeagueID=00&Location=&MeasureType=Opponent&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N' \
           '&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2015-16' \
-          '&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=' \
-          '&VsDivision=&Weight='
+          '&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight='
     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     while response.status_code != 200:
         response = requests.get(url)
@@ -67,7 +66,7 @@ def get_hustle_stats():
     url = 'http://stats.nba.com/stats/leaguehustlestatsplayer?College=&Conference=&Country=&DateFrom=&DateTo=' \
           '&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=' \
           '&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=' \
-          '&PlayerPosition=&PlusMinus=N&Rank=N&Season=2015-16&SeasonSegment=&SeasonType=Regular+Season' \
+          '&PlayerPosition=&PlusMinus=N&Rank=N&Season=2015-16&SeasonSegment=&SeasonType=Playoffs' \
           '&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight='
     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     while response.status_code != 200:
@@ -81,7 +80,7 @@ def get_overall_defense():
     url = 'http://stats.nba.com/stats/leaguedashptstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=' \
           '&DraftPick=&DraftYear=&GameScope=&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0' \
           '&Outcome=&PORound=0&PerMode=PerGame&PlayerExperience=&PlayerOrTeam=Player&PlayerPosition=' \
-          '&PtMeasureType=Defense&Season=2015-16&SeasonSegment=&SeasonType=Regular+Season&StarterBench=' \
+          '&PtMeasureType=Defense&Season=2015-16&SeasonSegment=&SeasonType=Playoffs&StarterBench=' \
           '&TeamID=0&VsConference=&VsDivision=&Weight='
     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     while response.status_code != 200:
@@ -95,7 +94,7 @@ def get_speed_and_distance():
     url = 'http://stats.nba.com/stats/leaguedashptstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=' \
           '&DraftPick=&DraftYear=&GameScope=&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0' \
           '&Outcome=&PORound=0&PerMode=PerGame&PlayerExperience=&PlayerOrTeam=Player&PlayerPosition=' \
-          '&PtMeasureType=SpeedDistance&Season=2015-16&SeasonSegment=&SeasonType=Regular+Season&StarterBench=' \
+          '&PtMeasureType=SpeedDistance&Season=2015-16&SeasonSegment=&SeasonType=Playoffs&StarterBench=' \
           '&TeamID=0&VsConference=&VsDivision=&Weight='
     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     while response.status_code != 200:
@@ -104,6 +103,29 @@ def get_speed_and_distance():
     data = response.json()['resultSets'][0]['rowSet']
     frame = pd.DataFrame(data, columns=headers)
     return frame
+
+def get_player_info(player_id):
+    url = 'http://stats.nba.com/stats/commonplayerinfo?LeagueID=00' \
+          '&PlayerID=' + str(player_id) + '&SeasonType=Regular+Season'
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    while response.status_code != 200:
+        response = requests.get(url)
+    headers = response.json()['resultSets'][0]['headers']
+    data = response.json()['resultSets'][0]['rowSet']
+    frame = pd.DataFrame(data, columns=headers)
+    return frame
+
+def get_players():
+    data = pd.read_csv('./data/hustle.csv')
+    players = data['PLAYER_ID'].values
+
+    player_df = []
+
+    for player in players:
+        player_data = get_player_info(player)
+        player_df.append(player_data)
+
+    player_df.to_csv('./data/players.csv')
 
 
 
--- population/population.py
+++ population/population.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import api
 
-frame = api.get_hustle_stats()
-frame.to_csv('data/hustle.csv', index=False)
-
-frame = api.get_rim_protection()
-frame.to_csv('data/rim.csv', index=False)
-
-frame = api.get_opponent_shooting_by_zone()
-frame.to_csv('data/zone_shooting.csv', index=False)
-
-frame = api.get_overall_defense()
-frame.to_csv('data/overall_defense.csv', index=False)
-
-frame = api.get_speed_and_distance()
-frame.to_csv('data/speed.csv', index=False)
+# frame = api.get_hustle_stats()
+# frame.to_csv('data/hustle.csv', index=False)
+#
+# # frame = api.get_rim_protection()
+# # frame.to_csv('data/rim.csv', index=False)
+#
+# frame = api.get_opponent_shooting_by_zone()
+# frame.to_csv('data/zone_shooting.csv', index=False)
+#
+# frame = api.get_overall_defense()
+# frame.to_csv('data/overall_defense.csv', index=False)
+#
+# frame = api.get_speed_and_distance()
+# frame.to_csv('data/speed.csv', index=False)
+api.get_players()
