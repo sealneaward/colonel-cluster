6b4bbb4c54cc02ee28d5936a4ba9e86eab4a8ca9
Neil Seward <neil.seward@outlook.com>
2016-12-13 10:16:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt-get install python-pip python-dev build-essential
 apt-get install python-psycopg2 python-sklearn python-matplotlib
+apt-get install python-pandas
 apt-get install postgresql
 apt-get install pgadmin3
 
